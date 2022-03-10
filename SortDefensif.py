1ed504830b52527f997cf33f4658e5ec4a870098
Alexis Faure <alexisfaure03@icloud.com>
2022-03-10 15:00:41 +0100

    Create SortDefensif.py

---
+++ SortDefensif.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 12:16:39 2022
+
+@author: pthuringer
+"""
+from Sort import Sort
+
+class SortDefensif(Sort):
+    def __init__(self,nom, pdr):
+        Sort.__init__(self)
