c5597d4b61e28eba1862ba98a5e81e42c96fcf88
Alexis Faure <alexisfaure03@icloud.com>
2022-03-10 15:00:46 +0100

    Create SortSournois.py

---
+++ SortSournois.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 12:17:38 2022
+
+@author: pthuringer
+"""
+
+from Sort import Sort
+
+class SortSournois(Sort):
+    def __init__(self,nom, pdd):
+        Sort.__init__(self)
