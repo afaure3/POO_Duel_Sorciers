e9414da5a4bebd950327ab2b61130bad1ea41557
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-10 15:34:48 +0100

    Update SortOffensif.py

--- SortOffensif.py
+++ SortOffensif.py
@@ -10,3 +10,4 @@ from Sort import Sort
 class SortOffensif(Sort):
     def __init__(self,nom, pdd):
         Sort.__init__(self)
+        self.dégat = 3    # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
