bc63dae338fdee1a69d914b303e99eb7ff0b8ffb
Alexis Faure <alexisfaure03@icloud.com>
2022-03-10 15:00:38 +0100

    Create Sort.py

---
+++ Sort.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 12:12:55 2022
+
+@author: pthuringer
+"""
+
+
+class Sort:
+
+    def __init__(self,nom, type, pdd, pdr):
+        self.nom = getNom
+        self.type = getType
+        self.pdd = getPDD
+        self.pdr = getPDR
+    
+    def getNom(self):
+        nom = sortOffensif
+    
+    def getType(self):
+        #retourne le type du sort
+    
+    def getPDD(self):
+        #retourne le nombre de points de dégâts du sort
+    
+    def getPDR(self):
+        #initialise le nombre de points de récupération du sort
