993e08b3a79efa9eead9d279c9ea02be3916a9cf
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-10 15:40:35 +0100

    Update SortDefensif.py

--- SortDefensif.py
+++ SortDefensif.py
@@ -10,3 +10,4 @@ from Sort import Sort
 class SortDefensif(Sort):
     def __init__(self,nom, pdr):
         Sort.__init__(self)
+        self.dégats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier + pas sûr du nombre de dégats 
