35ce00baa5fc001fca5e92bf67e7070b5e7543c1
Alexis Faure <alexisfaure03@icloud.com>
2022-03-10 15:00:43 +0100

    Create SortOffensif.py

---
+++ SortOffensif.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 12:15:24 2022
+
+@author: pthuringer
+"""
+from Sort import Sort
+
+class SortOffensif(Sort):
+    def __init__(self,nom, pdd):
+        Sort.__init__(self)
