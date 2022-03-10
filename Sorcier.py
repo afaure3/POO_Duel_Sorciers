90c49ae3704d77400dc1d1dbd810bf60f2118057
Alexis Faure <alexisfaure03@icloud.com>
2022-03-10 15:00:36 +0100

    Create Sorcier.py

---
+++ Sorcier.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 12:08:33 2022
+
+@author: pthuringer
+"""
+
+
+class Sorcier :
+
+
+    def __init__(self,nom):
+        #initialise le nom du sorcier
+     
+    def getNom(self):
+        #retourne le nom du sorcier
+        
+
+    def getPDV(self):
+        #retourne le nombre de points de vie du sorcier
+    
+    def setPDV(self,pdv):
+        #modifie le nombre de points de vie du sorcier
+    
+    def lancerSort(self,sort):
+        
+		#affiche : [nom du sorcier] lance le sort [nom du sort] de type [type du sort]
+        #si le sort est de type offensif ou sournois, affiche : avec [nombre de points de dégâts du sort] points de dégât
+        #sinon si le sort est de type défensif, affiche : avec [nombre de points de récupération] points de récupération
+
+    def recupererPDV(self,nbPdv):
+       #ajoute nbPdv points à la jauge de points de vie du sorcier
+    
+    def perdrePDV(self,nbPdv):
+       #enlève nbPdv points à la jauge de points de vie du sorcier
+    
+    
