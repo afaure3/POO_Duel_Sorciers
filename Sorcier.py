2a7b6d4d7a46f1892a665734340cb9a660412376
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-19 21:22:43 +0100

    Update Sorcier.py

--- Sorcier.py
+++ Sorcier.py
@@ -6,6 +6,7 @@ Created on Sun Jan  2 12:08:33 2022
 @author: pthuringer
 """
 
+from Sort import Sort
 
 class Sorcier :
 
@@ -29,18 +30,20 @@ class Sorcier :
         self.PDV = 5
     
     def lancerSort(self,sort):
-        if type(self.sort) == "sournois": 
-            print(self.nom + "lance le sort " (sort.nom) + type(self.sort))
-        if type(self.sort) == "offensif":
-            print(self.nom + "lance le sort " (sort. nom) + type(self.sort))
-        else : print(self.nom + "lance le sort" + type(self.sort) + " avec" + (sort.pdr) + " points de récupération.")
+        if Sort.getType(self) == "offensif" or "sournois" : 
+            print(self.nom + "lance le sort " + Sort.getNom(self) + " de type " + Sort.getType(self) + " avec " Sort.getPDD(self) + " points de dégats.")
+        else print(self.nom + "lance le sort " + Sort.getNom(self) + " de type " + Sort.getType(self) + " avec " Sort.getPDR(self) + " points de récupération.")
 		#affiche : [nom du sorcier] lance le sort [nom du sort] de type [type du sort]
         #si le sort est de type offensif ou sournois, affiche : avec [nombre de points de dégâts du sort] points de dégât
         #sinon si le sort est de type défensif, affiche : avec [nombre de points de récupération] points de récupération
 
     def recupererPDV(self,nbPdv):
        #ajoute nbPdv points à la jauge de points de vie du sorcier
-        
+       self.nbPdv = Sort.getPDR(self)
+       self.PDV = + self.nbPDV 
+       
     
-    def perdrePDV(self,nbPdv)
+    def perdrePDV(self,nbPdv): 
        #enlève nbPdv points à la jauge de points de vie du sorcier
+       self.nbPdv = Sort.getPDV(self)
+       self.PDV = - self.nbPDV 
