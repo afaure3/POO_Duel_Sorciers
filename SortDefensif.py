6928c4d22b2aca794db076d143d3618f62487792
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-19 20:31:11 +0100

    Update SortDefensif.py

--- SortDefensif.py
+++ SortDefensif.py
@@ -7,10 +7,10 @@ Created on Sun Jan  2 12:16:39 2022
 """
 from Sort import Sort
 
-class SortDefensif(Sort):
-    def __init__(self,nom, pdr):
-        Sort.__init__(self)
-        self.degats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier + pas sûr du nombre de dégats
+#class SortDefensif(Sort):
+ #   def __init__(self,nom, pdr):
+  #      Sort.__init__(self)
+   #     self.degats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier + pas sûr du nombre de dégats
 
 class SortDefensif(Sort):
     def __init__(self,nom, pdr):
