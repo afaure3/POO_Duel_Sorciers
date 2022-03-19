f6026e35a8c1dbb72e9a448226fe4763ebd34810
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-19 20:31:18 +0100

    Update SortSournois.py

--- SortSournois.py
+++ SortSournois.py
@@ -8,10 +8,10 @@ Created on Sun Jan  2 12:17:38 2022
 
 from Sort import Sort
 
-class SortSournois(Sort):
-    def __init__(self,nom, pdd):
-        Sort.__init__(self)
-        self.degats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
+#class SortSournois(Sort):
+ #   def __init__(self,nom, pdd):
+  #      Sort.__init__(self)
+   #     self.degats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
 
 class SortSournois(Sort):
     def __init__(self,nom, pdd):
