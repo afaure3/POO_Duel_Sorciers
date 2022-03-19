21c809e25c30377fac04725632f4e768a514922e
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-03-19 20:31:14 +0100

    Update SortOffensif.py

--- SortOffensif.py
+++ SortOffensif.py
@@ -7,10 +7,10 @@ Created on Sun Jan  2 12:15:24 2022
 """
 from Sort import Sort
 
-class SortOffensif(Sort):
-    def __init__(self,nom, pdd):
-        Sort.__init__(self)
-        self.degats = 2    # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
+#class SortOffensif(Sort):
+    #def __init__(self,nom, pdd):
+       #Sort.__init__(self)
+        #self.degats = 2    # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
 
 class SortOffensif(Sort):
     def __init__(self,nom, pdd):
