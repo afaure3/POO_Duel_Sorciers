cba284d42fb3c3c9dcd7b4da504ae7af520d92a8
Alexis Faure <alexisfaure03@icloud.com>
2022-03-21 17:18:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ from Duel import Duel
 def main():
     nom1=input("Veuillez saisir le nom du sorcier 1 : ")
     #Instanciation du premier sorcier
-    sorcier1 =  Sorcier(nom1);
+    sorcier1 =  Sorcier(nom1)
 
-    nom2=input("Veuillez saisir le nom du sorcier 2 : ");
+    nom2=input("Veuillez saisir le nom du sorcier 2 : ")
     #Instanciation du second sorcier
-    sorcier2 = Sorcier(nom2);
+    sorcier2 = Sorcier(nom2)
 
-    print("\nLe duel peut commencer\n") ;
+    print("\nLe duel peut commencer\n")
     #Instanciation du duel
-    duel=Duel(sorcier1, sorcier2);
+    duel=Duel(sorcier1, sorcier2)
 
     #tant que le duel est en cours
     while duel.getEtat()=="en cours":
@@ -40,12 +40,12 @@ def main():
         #si le nombre de points de vie de chaque sorciers est strictement supérieur à 0
         if score[0]>0 and score[1]>0:
             #le duel continue on passe au tour suivant
-            duel.tourSuivant();
+            duel.tourSuivant()
         #sinon
         else:
             #le duel se termine
-            duel.setEtat("terminé");
+            duel.setEtat("terminé")
             #on affiche le résumé
-            duel.afficherResumeDuel();
+            duel.afficherResumeDuel()
         
 main()
