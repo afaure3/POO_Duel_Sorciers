5dba0d930ae736aa97291ffe831eb33a257cb4bd
Alexis Faure <alexisfaure03@icloud.com>
2022-03-30 18:27:40 +0200

    Update SortSournois.py

--- SortSournois.py
+++ SortSournois.py
@@ -8,11 +8,6 @@ Created on Sun Jan  2 12:17:38 2022
 
 from Sort import Sort
 
-#class SortSournois(Sort):
- #   def __init__(self,nom, pdd):
-  #      Sort.__init__(self)
-   #     self.degats = 2 # à voir quand on sera plus loin si plus complexe ou pris en charge par un autre fichier
-
 class SortSournois(Sort):
     def __init__(self,nom, pdd, pdr):
         type = "sournois"
