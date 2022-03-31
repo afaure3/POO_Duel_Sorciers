8b34ccdd4a1573ba9a1aaeecf0cc1d9ea42aeb55
Alexis Faure <alexisfaure03@icloud.com>
2022-03-31 19:11:49 +0200

    Update SortOffensif.py

--- SortOffensif.py
+++ SortOffensif.py
@@ -8,7 +8,7 @@ Created on Sun Jan  2 12:15:24 2022
 from Sort import Sort
 
 class SortOffensif(Sort):
-    def __init__(self,nom, pdd, pdr):
-        type =  "offensif"
-        pdr = 0
-        Sort.__init__(self,nom, type, pdd, pdr)
+    def __init__(self, nom, pdd):
+        Sort.__init__(self, nom, "offensif", pdd, 0)
+        self.nom = nom
+        self.pdd = pdd
