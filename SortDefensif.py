c9d14d8394e2af29a90fa9790ab8fca31cb37877
Alexis Faure <alexisfaure03@icloud.com>
2022-03-31 19:11:51 +0200

    Update SortDefensif.py

--- SortDefensif.py
+++ SortDefensif.py
@@ -8,7 +8,7 @@ Created on Sun Jan  2 12:16:39 2022
 from Sort import Sort
 
 class SortDefensif(Sort):
-    def __init__(self,nom, pdr, pdd):
-        type =  "défensif"
-        pdr = 0
-        Sort.__init__(self,nom, type, pdr, pdd)
+    def __init__(self, nom, pdr):
+        Sort.__init__(self, nom, "défensif", pdr, 0)
+        self.nom = nom
+        self.pdr = pdr
