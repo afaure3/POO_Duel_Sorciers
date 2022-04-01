a98ddadb953b3478528137650f5d9c9f8a365bed
Alexis Faure <alexisfaure03@icloud.com>
2022-04-01 15:46:17 +0200

    Update Tour.py

--- Tour.py
+++ Tour.py
@@ -35,16 +35,16 @@ class Tour:
             while(True):
                 saisiesorcier1 = input('Choisir un sort offensif : 1 pour Flipendo, 2 pour Impedimenta, 3 pour Crache limace : ')
                 if saisiesorcier1 == "1":
-                    sortoffensif = SortOffensif('Flipendo', 4)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Flipendo', 4)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "2":
-                    sortoffensif = SortOffensif('Impedimenta', 5)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Impedimenta', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "3":
-                    sortoffensif = SortOffensif('Crache Limace', 5)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Crache Limace', 5)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie, veuillez recommencer.")
@@ -52,16 +52,16 @@ class Tour:
             while(True):
                 saisiesorcier1 = input('Choisir un sort sournois : 1 pour Legilimens, 2 pour Morsmordre, 3 pour Oubliettes : ')
                 if saisiesorcier1 == "1":
-                    sortsournois = SortSournois('Legilimens', 5)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Legilimens', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "2":
-                    sortsournois = SortSournois('Morsmordre', 3)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Morsmordre', 3)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "3":
-                    sortsournois = SortSournois('Oubliettes', 4)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Oubliettes', 4)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie, veuillez recommencer") 
@@ -69,19 +69,21 @@ class Tour:
             while(True):
                 saisiesorcier1 = input("Choisir un sort défensif : 1 pour Expelliarmus, 2 pour Protego, 3 pour Spero Patronum : ")
                 if saisiesorcier1 == "1":
-                    sortdefensif = SortDefensif('Expelliarmus', 5)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Expelliarmus', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "2":
-                    sortdefensif = SortDefensif('Protego', 3)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Protego', 3)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier1 == "3":
-                    sortdefensif = SortDefensif('Spero Patronum', 4)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Spero Patronum', 4)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie.")
+
+        self.sorciers[0].lancerSort(choixsort)
 
     def tourSorcier2(self):
         print("-", self.sorciers[1].getNom(), " : ")
@@ -89,16 +91,16 @@ class Tour:
             while(True):
                 saisiesorcier2 = input("Choisir un sort défensif : 1 pour Expelliarmus, 2 pour Protego, 3 pour Spero Patronum : ")
                 if saisiesorcier2 == "1":
-                    sortdefensif = SortDefensif('Expelliarmus', 5)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Expelliarmus', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "2":
-                    sortdefensif = SortDefensif('Protego', 3)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Protego', 3)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "3":
-                    sortdefensif = SortDefensif('Spero Patronum', 4)
-                    self.sorts.append(sortdefensif)
+                    choixsort = SortDefensif('Spero Patronum', 4)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie.")
@@ -106,16 +108,16 @@ class Tour:
             while(True):
                 saisiesorcier2 = input('Choisir un sort offensif : 1 pour Flipendo, 2 pour Impedimenta, 3 pour Crache limace : ')
                 if saisiesorcier2 == "1":
-                    sortoffensif = SortOffensif('Flipendo', 4)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Flipendo', 4)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "2":
-                    sortoffensif = SortOffensif('Impedimenta', 5)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Impedimenta', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "3":
-                    sortoffensif = SortOffensif('Crache Limace', 5)
-                    self.sorts.append(sortoffensif)
+                    choixsort = SortOffensif('Crache Limace', 5)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie, veuillez recommencer.")
@@ -123,19 +125,21 @@ class Tour:
             while(True):
                 saisiesorcier2 = input('Choisir un sort sournois : 1 pour Legilimens, 2 pour Morsmordre, 3 pour Oubliettes : ')
                 if saisiesorcier2 == "1":
-                    sortsournois = SortSournois('Legilimens', 5)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Legilimens', 5)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "2":
-                    sortsournois = SortSournois('Morsmordre', 3)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Morsmordre', 3)
+                    self.sorts.append(choixsort)
                     break
                 elif saisiesorcier2 == "3":
-                    sortsournois = SortSournois('Oubliettes', 4)
-                    self.sorts.append(sortsournois)
+                    choixsort = SortSournois('Oubliettes', 4)
+                    self.sorts.append(choixsort)
                     break
                 else:
                     print("Mauvaise saisie, veuillez recommencer")
+
+        self.sorciers[1].lancerSort(choixsort)
 
     def calculerPDV(self):
         type1 = self.sorts[0].getType()
@@ -190,6 +194,10 @@ class Tour:
 
         score1 = self.sorciers[0].getPDV()
         score2 = self.sorciers[1].getPDV()
+        if score1 < 0:
+            score1 = 0
+        if score2 < 0:
+            score2 = 0
         self.scores = [score1, score2]
     
 
@@ -197,5 +205,9 @@ class Tour:
         print(Tour.getScore(self))
 
     def afficherResumeTour(self):
-        print('Tour', Tour.getNumero(self))
+        print('Tour')
+        self.getNumero()
+        print('Le sorcier', self.sorciers[0].nom, "a lancé", self.sorts[0].nom)
+        print('Le sorcier', self.sorciers[1].nom, "a lancé", self.sorts[1].nom)
+        print('Le score actuel est :')
         self.afficherScoreTour()
