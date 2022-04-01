d1e8afd8b4a765cf34b6a68de1bd43eddb0db8d8
Alexis Faure <alexisfaure03@icloud.com>
2022-04-01 17:50:14 +0200

    Update Duel.py

--- Duel.py
+++ Duel.py
@@ -12,40 +12,40 @@ from Tour import Tour
 class Duel:
 
     def __init__(self,sorcier1, sorcier2):
-        self.sorcier1 = sorcier1
-        self.sorcier2 = sorcier2
-        self.sorcier1.setPDV(5)
-        self.sorcier2.setPDV(5)
-        self.tourCourant = Tour(1, sorcier1, sorcier2)
-        self.tours = [self.tourCourant]
-        self.etat = "en cours"
+        self.__sorcier1 = sorcier1
+        self.__sorcier2 = sorcier2
+        self.__sorcier1.setPDV(5)
+        self.__sorcier2.setPDV(5)
+        self.__tourCourant = Tour(1, sorcier1, sorcier2)
+        self.__tours = [self.__tourCourant]
+        self.__etat = "en cours"
     
     def getTourCourant(self):
-        return self.tourCourant
+        return self.__tourCourant
     
     def getEtat(self):
-        return self.etat
+        return self.__etat
     
     def setEtat(self, etat):
-        self.etat = etat
+        self.__etat = etat
         
      
     def tourSuivant(self):
-        numeroSuivant = self.tourCourant.getNumero() + 1
-        tourSuivant = Tour(numeroSuivant, self.sorcier1, self.sorcier2)
-        self.tourCourant = tourSuivant
-        self.tours.append(self.tourCourant)
+        numeroSuivant = self.__tourCourant.getNumero() + 1
+        tourSuivant = Tour(numeroSuivant, self.__sorcier1, self.__sorcier2)
+        self.__tourCourant = tourSuivant
+        self.__tours.append(self.__tourCourant)
     
     def determinerVainqueur(self):
-        Tour.getScore(self)
-        if Tour.scores[0] == 0:
+        self.__tourCourant.getScore()
+        if self.__tourCourant.scores[0] == 0 and self.__tourCourant.scores[1] > 0:
             print("le sorcier 2 gagne le duel !")
-        elif Tour.scores[1] == 0:
+        elif self.__tourCourant.scores[1] == 0 and self.__tourCourant.scores[0] > 0:
             print("Le sorcier 1 a gagné le duel !")
-        elif Tour.scores[0] == 0 and Tour.scores[1] == 0:
+        elif self.__tourCourant.scores[0] == 0 and self.__tourCourant.scores[1] == 0:
             print("Match nul ! Les deux sorciers ont perdu...")
 
     def afficherResumeDuel(self):
         print('Le duel est terminé. Résumé du duel : ')
-        Tour.afficherResumeTour(self)
+        self.__tourCourant.afficherResumeTour()
         self.determinerVainqueur()
